8b657fe1abe26a3923f3adcfc4da1d3a4066f1fd
fleshold <fleshold@yandex.ru>
2023-06-12 18:37:29 +0300

    картинки

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const n = (sel, b = document.body) => {
+    
+    const n = b.querySelector(sel);
+    if (!n) throw new Error(`Не найден: ${b.id} -> ${sel}`);
+    
+    return n;
+};
