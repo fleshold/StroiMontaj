fd76cab53ec5c0e6419eb8924350258ca3b085b7
fleshold <fleshold@yandex.ru>
2023-07-10 15:04:10 +0300

    convert to php navs ja headers

---
+++ tmpl/navigation.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.html">КОМПАНИЯ СТРОЙМОНТАЖ</a>                    
+                    <a class="navbar-brand bi bi-telephone" href="tel:+79810236545"><span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> +7 (981) 023-65-45</a>
+                    <a class="navbar-brand bi bi-telephone" href="tel:+74942544438"><span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> +7 (4942) 54-44-38</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.html">Главная</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Услуги</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                    <li><a class="dropdown-item" href="services-home.html">Все услуги</a></li>
+                                    <li><a class="dropdown-item" href="services-post1.html">1. Строительство зданий и сооружений</a></li>
+                                    <li><a class="dropdown-item" href="services-post2.html">2. Изготовление металлических ограждений</a></li>
+                                    <li><a class="dropdown-item" href="services-post3.html">3. Изготовление металлических ферм</a></li>
+                                    <li><a class="dropdown-item" href="services-post4.html">4. Изготовление металлических колонн</a></li>
+                                    <li><a class="dropdown-item" href="services-post5.html">5. Изготовление закладных деталей на заказ</a></li>
+                                    <li><a class="dropdown-item" href="services-post6.html">6. Изготовление навесов</a></li>
+                                   
+                                </ul>
+                            </li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">О компании</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Контакты</a></li>    
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="container px-5 .my-0">
+                <blockquote class="blockquote">
+                    <p>
+                        <span class="border-bottom">Оставьте заявку на <a href="contact.html">сайте</a>. Или звоните по указанным выше телефонам. Мы свяжемся с Вами для уточнения деталей и заключению договора.
+                        </span>
+                    </p>
+                </blockquote> 
+            </div>
